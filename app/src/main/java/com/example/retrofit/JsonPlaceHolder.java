d388dc52e3ce08c22a213ab146692b57571eed71
Artur <arturstasicki@gmail.com>
2022-07-25 11:56:39 +0200

    Define path v1

--- app/src/main/java/com/example/retrofit/JsonPlaceHolder.java
+++ app/src/main/java/com/example/retrofit/JsonPlaceHolder.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Part;
 
 public interface JsonPlaceHolder {
 
     @GET("posts")
     Call<List<Post>> getPost();
 
-    @GET("posts/2/comments")
-    Call<List<Comment>> getComments();
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Part("id") int postId);
 }
--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     private void getComments(){
-        Call<List<Comment>> call = jsonPlaceHolder.getComments();
+        Call<List<Comment>> call = jsonPlaceHolder.getComments(3);
 
         call.enqueue(new Callback<List<Comment>>() {
             @Override
