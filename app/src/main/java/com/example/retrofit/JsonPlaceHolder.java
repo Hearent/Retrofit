fe9d29a91d59d833bf7b8339f47ced8f64243421
Artur <arturstasicki@gmail.com>
2022-07-25 09:42:02 +0200

    Create body of interface JsonPlaceHolder

--- app/src/main/java/com/example/retrofit/JsonPlaceHolder.java
+++ app/src/main/java/com/example/retrofit/JsonPlaceHolder.java
@@ -1,4 +1,12 @@
 package com.example.retrofit;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface JsonPlaceHolder {
+
+    @GET("posts")
+    Call<List<Post>> getPost();
 }
